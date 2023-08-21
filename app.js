67b416537588d80594d485c9504969cc00259a51
Erik S <eriks@Eriks-MacBook-Pro.local>
2023-08-21 14:06:48 +0200

    changed line 5 again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('erik var her'));
+app.get('/', (req, res) => res.send('erik var her. erik var her igjen'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
